2c46e41e53ff318c84dd20f0f64fe9348bdc436a
ari <aribennett1@gmail.com>
2021-10-25 16:13:52 -0400

    Created ClientGenerator class, I think I finished the the Client class, please let me know what you guys think

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,4 +1,7 @@
-public class Client<jobType> {
+import java.io.*;
+import java.net.Socket;
+
+public class Client extends Thread {
     private int id;
     private JobType jobType;
 
@@ -7,11 +10,32 @@ public class Client<jobType> {
         this.jobType = jobType;
     }
 
-    public int getId() {
+    public int getJobId() {
         return id;
     }
 
     public JobType getJobType() {
         return jobType;
+    }
+
+    public void run() {
+        int port = 12345; //randomly chosen, we should decide on a common port to use
+        int serverResponse;
+        try (Socket clientSocket = new Socket("127.0.0.1", port)
+        ) {
+            System.out.println("Setting up the streams...");
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println("" + getId() + "|" + getJobType());
+            if ((serverResponse = in.read() /*expects an int*/) == getJobId()) {
+                System.out.println("Received " + serverResponse + " as confirmation that Server finished client number " + getId());
+            }
+            else {
+                System.out.println("Error, Server response was " + serverResponse + ", job ID is " + getId());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
