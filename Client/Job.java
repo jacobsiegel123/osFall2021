503247bcfb655917d83aba250229fc1a41f6051e
aribennett1 <70670877+aribennett1@users.noreply.github.com>
2021-11-10 16:19:02 -0500

    Add files via upload

---
+++ Client/Job.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+
+public class Job extends Thread {
+    private int id;
+    private JobType jobType;
+    private ServerSocket serverSocket;
+
+    public Job(int id, JobType jobType, ServerSocket serverSocket) {
+        this.id = id;
+        this.jobType = jobType;
+        this.serverSocket = serverSocket;
+    }
+
+    public int getJobId() {
+        return id;
+    }
+
+    public JobType getJobType() {
+        return jobType;
+    }
+
+    public void run() {
+        int masterResponse;
+        try {
+            Socket clientSocket = serverSocket.accept();
+            System.out.print("Setting up the streams " + getId() + "... ::");
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println("" + getId() + "|" + getJobType());
+            if ((masterResponse = in.read()/*expects an int*/) == getJobId()) {
+                System.out.println("Received " + masterResponse + " as confirmation that Server finished client number " + getId());
+            } else {
+                System.out.println("Error, Server response was " + masterResponse + ", job ID is " + getId());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
