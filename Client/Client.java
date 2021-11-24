1176a726dcd3a6e16d8903ac53e089817ff1cbe3
aribennett1 <70670877+aribennett1@users.noreply.github.com>
2021-11-23 20:16:25 -0500

    Add files via upload

--- Client/Client.java
+++ Client/Client.java
@@ -1,15 +1,22 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
     private static final int MAX_THREADS = 10; //randomly chosen, could be changed at any time
-    private static final String clientID = setClientID();
 
     public static void main(String[] args) throws IOException {
         Thread[] threads = new Thread[MAX_THREADS];
-        int port = 12345; //randomly chosen, we should decide on a common port to use
-        ServerSocket serverSocket = new ServerSocket(port);
+        int masterPort = 12345; //randomly chosen, we should decide on a common port to use
+        int responsePort;
+        if (args[0] != null) {
+        responsePort = Integer.parseInt(args[0]);}
+        else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the first response port number: ");
+            responsePort = sc.nextInt();
+        }
+        ServerSocket serverSocket = new ServerSocket(masterPort);
         int jobID = 1;
         //Keep checking threads, if a job has completed, give the thread a new job
         for (int i = 0; i <= threads.length; i++) {
@@ -18,37 +25,19 @@ public class Client {
                 i = 0;
             }
             if (threads[i] == null || !threads[i].isAlive()) {
-                startJob(threads[i], jobID++, serverSocket);
+                startJob(threads[i], jobID++, serverSocket, responsePort + i);
             }
         }
     }
 
-    private static void startJob(Thread thread, int jobID, ServerSocket serverSocket) {
+    private static void startJob(Thread thread, int jobID, ServerSocket serverSocket, int responsePort) {
         JobType jobType;
         if (Math.random() > .5) {
             jobType = JobType.JOB_A;
         } else {
             jobType = JobType.JOB_B;
         }
-        String id = getClientID() + "_" + jobID;
-        thread = new Job(id, jobType, serverSocket);
+        thread = new Job(jobID, jobType, serverSocket, responsePort);
         thread.start();
-    }
-
-    private static String setClientID() {
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(3);
-        for (int i = 0; i < 3; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        return buffer.toString();
-    }
-
-    public static String getClientID() {
-        return clientID;
     }
 }
--- Client/Job.java
+++ Client/Job.java
@@ -2,22 +2,24 @@ import java.io.*;
 import java.net.*;
 
 public class Job extends Thread {
-    private String id;
+    private int id;
     private JobType jobType;
     private ServerSocket serverSocket;
+    private int responsePort;
 
-    public Job(String id, JobType jobType, ServerSocket serverSocket) {
+    public Job(int id, JobType jobType, ServerSocket serverSocket, int responsePort) {
         this.id = id;
         this.jobType = jobType;
         this.serverSocket = serverSocket;
+        this.responsePort = responsePort;
     }
 
     public int getJobId() {
-        return Integer.parseInt(id.substring(id.indexOf("_" + 1)));
-    }
-    
-    public String getFullId() {
         return id;
+    }
+
+    public int getResponsePort() {
+        return responsePort;
     }
 
     public JobType getJobType() {
@@ -27,11 +29,13 @@ public class Job extends Thread {
     public void run() {
         int masterResponse;
         try {
+            ServerSocket responseSocket = new ServerSocket(responsePort);
             Socket clientSocket = serverSocket.accept();
+            Socket responseClient = responseSocket.accept();
             System.out.print("Setting up the streams " + getId() + "... ::");
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out.println(getFullId() + "|" + getJobType());
+            BufferedReader in = new BufferedReader(new InputStreamReader(responseClient.getInputStream()));
+            out.println(getResponsePort() + "|" + getJobId() + "||" + getJobType());
             if ((masterResponse = in.read()/*expects an int*/) == getJobId()) {
                 System.out.println("Received " + masterResponse + " as confirmation that Server finished client number " + getJobId());
             } else {
